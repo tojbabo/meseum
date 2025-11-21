723e8766b99f184a54ae96ab0b385d05fee01688
one joon <tojbabo@gmail.com>
2025-11-21 22:24:05 +0900

    feat(b): json file read 설정 및 버전 표기 추가

--- backend/dist/main.js
+++ backend/dist/main.js
@@ -2,7 +2,10 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
+const common_1 = require("@nestjs/common");
+const logger = new common_1.Logger('main');
 async function bootstrap() {
+    logger.log('Starting server... ');
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     await app.listen(process.env.PORT ?? 3000);
 }
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,7 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import * as pkg from '../package.json';
+
+
+const logger = new Logger('main');
 
 async function bootstrap() {
+  logger.log(`Starting server... ${pkg.name} v${pkg.version}`);
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
 }
