03b6408c4b1c639e763986e1aef156e72207b4d1
one joon <tojbabo@gmail.com>
2025-11-24 22:59:56 +0900

    docs(b): cd test

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,7 +7,7 @@ import * as pkg from '../package.json';
 const logger = new Logger('main');
 
 async function bootstrap() {
-  logger.log(`Starting server... ${pkg.name} v${pkg.version}`);
+  logger.log(`Starting server... hi ${pkg.name} v${pkg.version}`);
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
 }
