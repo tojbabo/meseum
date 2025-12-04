3d8fc5ee6ae97c72eaa50edc7d212a39338bbf35
one joon <tojbabo@gmail.com>
2025-12-04 23:59:33 +0900

    feat(b): 쿠키 설정으로 해당 사용자가 첫방문이지 다회 방문인지 식별

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,8 +1,13 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Body, Req, Res, HttpVersionNotSupportedException } from '@nestjs/common';
 import { AppService } from 'src/app.service';
+import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc';
+import timezone from 'dayjs/plugin/timezone';
 
 @Controller()
 export class AppController {
+  private today_count = 0;
+  
   constructor(private readonly appService: AppService) {}
 
   @Get()
@@ -11,7 +16,29 @@ export class AppController {
   }
 
   @Get('api/get')
-  getEntranceLocation(): object {
+  getEntranceLocation(@Req() req, @Res({passthrough: true}) res): object {// 1. 요청의 쿠키에서 플래그 확인
+    
+    const visitedToday = req.cookies['visited_today'];
+    dayjs.extend(utc);
+    dayjs.extend(timezone)
+    const nowKst = dayjs().tz('Asia/Seoul');
+
+    if(!visitedToday){
+      console.log('여기 왔니?')
+      this.today_count++;
+      const midnightKst = nowKst.endOf('day').toDate();
+
+      res.cookie('visited_today', 'true', {
+        expires: midnightKst,
+        httpOnly:true,
+        secure: true,
+        sameSite:'none'
+      })
+    }
+
+    console.log('today is: ', visitedToday, this.today_count);
+
+
     return this.appService.getEntranceLocation();
   }
 
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from 'src/app.module';
 import { Logger } from '@nestjs/common';
 import * as pkg from 'package.json';
+import * as cookieParser from "cookie-parser";
 
 const logger = new Logger('main');
 
@@ -9,11 +10,18 @@ async function bootstrap() {
   logger.log(`Starting server... hi2 ${pkg.name} v${pkg.version}`);
   const app = await NestFactory.create(AppModule);
 
+
   app.enableCors({
-    origin: '*',
+    origin: ['http://127.0.0.1', 'http://localhost'],
+    // origin: '*',
+    credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
   });
+  
+  app.use(cookieParser.default()); 
+
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
